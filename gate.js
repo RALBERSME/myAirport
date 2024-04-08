2976b4660cdfb4a4b9cb5ee77e4d1d43a7057540
RALBERSME <149826808+RALBERSME@users.noreply.github.com>
2024-04-08 21:53:55 +0200

    dots removed for speaker

--- gate.js
+++ gate.js
@@ -81,7 +81,7 @@ btn.addEventListener("click", func1);
 
 function func1() {
   const firstText =
-    "White Stork Traditional Airline welcomes you on your long eastern distance flight from Frankfurt to Cape Town in South Africa. We will be leaving on fifth of August at ten o`clock in the morning. We will travel at a speed of fifty kilometres per hour and make up to five handred kilometres per day. Please proceed to gate A twelve.";
+    "White Stork Traditional Airline welcomes you on your long eastern distance flight from Frankfurt to Cape Town in South Africa. We will be leaving on fifth of August at ten o`clock in the morning. We will travel at a speed of fifty kilometres per hour and make up to five handred kilometres per day. Please proceed to gate A twelve";
   const secondText =
     "We wish you a pleasant travel and look forward to your return in six months.";
   var msg = new SpeechSynthesisUtterance();
@@ -94,7 +94,7 @@ btn.addEventListener("click", func2);
 
 function func2() {
   const firstText =
-    "Mauersegler Pro Airline welcomes you on your long eastern distance flight from Frankfurt to Kinshasa in the Demokratic Republic Kongo. We will be leaving on sixteenth of August at two o`clock in the afternoon. We will travel at a speed of fourty kilometres per hour. Please proceed to gate A fifteen.";
+    "Mauersegler Pro Airline welcomes you on your long eastern distance flight from Frankfurt to Kinshasa in the Demokratic Republic Kongo. We will be leaving on sixteenth of August at two o`clock in the afternoon. We will travel at a speed of fourty kilometres per hour. Please proceed to gate A fifteen";
   const secondText =
     "We wish you a pleasant travel and look forward to your return in six months.";
   var msg = new SpeechSynthesisUtterance();
@@ -107,7 +107,7 @@ btn.addEventListener("click", func3);
 
 function func3() {
   const firstText =
-    "Mönchsgrasmücken Flights welcomes you on your short distance flight from Frankfurt to Murcia in Spain. We will be leaving on third September at four o`clock in the afternoon. We travel in a swarm of 50 planes. Please proceed to gate C three.";
+    "Mönchsgrasmücken Flights welcomes you on your short distance flight from Frankfurt to Murcia in Spain. We will be leaving on third September at four o`clock in the afternoon. We travel in a swarm of 50 planes. Please proceed to gate C three";
   const secondText =
     "We wish you a pleasant travel and look forward to your return in six months.";
   var msg = new SpeechSynthesisUtterance();
@@ -120,7 +120,7 @@ btn.addEventListener("click", func4);
 
 function func4() {
   const firstText =
-    "Chummy Star Alliance welcomes you on your middle distance flight from Frankfurt to Algier in Algeria. We will be leaving on Oktober tenth at six o`clock in the morning. We travel at a speed of sixty kilometres per hour. Please proceed to gate B thirty four.";
+    "Chummy Star Alliance welcomes you on your middle distance flight from Frankfurt to Algier in Algeria. We will be leaving on Oktober tenth at six o`clock in the morning. We travel at a speed of sixty kilometres per hour. Please proceed to gate B thirty four";
   const secondText =
     "We wish you a pleasant travel and look forward to your return in six months.";
   var msg = new SpeechSynthesisUtterance();
@@ -133,7 +133,7 @@ btn.addEventListener("click", func5);
 
 function func5() {
   const firstText =
-    "Kranich Flyaways  welcomes you on your short distance flight from Frankfurt to Merida in Spain. We will be leaving on Oktober elven at nine o`clock in the morning. We travel at a speed of fifty kilometres per hour. Please proceed to gate C three.";
+    "Kranich Flyaways  welcomes you on your short distance flight from Frankfurt to Merida in Spain. We will be leaving on Oktober elven at nine o`clock in the morning. We travel at a speed of fifty kilometres per hour. Please proceed to gate C three";
   const secondText =
     "We wish you a pleasant travel and look forward to your return in six months.";
   var msg = new SpeechSynthesisUtterance();
@@ -146,7 +146,7 @@ btn.addEventListener("click", func6);
 
 function func6() {
   const firstText =
-    "Wild Goose Air Taxis welcomes you on your middle distance flight from Frankfurt to Lissabon in Portugal. We will be leaving on Oktober eight at half past three in the morning. We travel at a speed of fifty kilometres per hour. Please proceed to gate B thirty four.";
+    "Wild Goose Air Taxis welcomes you on your middle distance flight from Frankfurt to Lissabon in Portugal. We will be leaving on Oktober eight at half past three in the morning. We travel at a speed of fifty kilometres per hour. Please proceed to gate B thirty four";
   const secondText =
     "We wish you a pleasant travel and look forward to your return in six months.";
   var msg = new SpeechSynthesisUtterance();
@@ -159,7 +159,7 @@ btn.addEventListener("click", func7);
 
 function func7() {
   const firstText =
-    "Mehl- & Rauchswalben Combi-Airline welcomes you on your long western distance flight from Frankfurt to Durban in South Africa. We will be leaving on second August at half past five in the morning. We travel at a speed of eighty kilometres per hour. Please proceed to gate A fifteen.";
+    "Mehl- & Rauchswalben Combi-Airline welcomes you on your long western distance flight from Frankfurt to Durban in South Africa. We will be leaving on second August at half past five in the morning. We travel at a speed of eighty kilometres per hour. Please proceed to gate A fifteen";
   const secondText =
     "We wish you a pleasant travel and look forward to your return in six months.";
   var msg = new SpeechSynthesisUtterance();
@@ -172,7 +172,7 @@ btn.addEventListener("click", func8);
 
 function func8() {
   const firstText =
-    "Pirol Shuttle welcomes you on your long western distance flight from Frankfurt to Brazzaville in the Republic Congo. We will be leaving on twenty fourth of July at a quarter to ten in the morning. Please proceed to gate A fifteen.";
+    "Pirol Shuttle welcomes you on your long western distance flight from Frankfurt to Brazzaville in the Republic Congo. We will be leaving on twenty fourth of July at a quarter to ten in the morning. Please proceed to gate A fifteen";
   const secondText =
     "We wish you a pleasant travel and look forward to your return in six months.";
   var msg = new SpeechSynthesisUtterance();
@@ -185,7 +185,7 @@ btn.addEventListener("click", func9);
 
 function func9() {
   const firstText =
-    "Adler Agile Airline welcomes you on your long eastern distance flight from Frankfurt to Lusaka in Sambia. We will be leaving on eleventh of September at noon. We travel at a speed of one hundred sixty kilometres per hour. Please proceed to gate A twelve.";
+    "Adler Agile Airline welcomes you on your long eastern distance flight from Frankfurt to Lusaka in Sambia. We will be leaving on eleventh of September at noon. We travel at a speed of one hundred sixty kilometres per hour. Please proceed to gate A twelve";
   const secondText =
     "We wish you a pleasant travel and look forward to your return in six months.";
   var msg = new SpeechSynthesisUtterance();
